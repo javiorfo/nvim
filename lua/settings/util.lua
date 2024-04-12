ed3857927c734ec4b79ec813b3761171c12b2d2c
Javier Orfo <124217829+javiorfo@users.noreply.github.com>
2024-04-12 19:42:32 -0300

    Update and rename init.lua to util.lua

--- lua/settings/util.lua
+++ lua/settings/util.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+function M.table_contains_string(tbl, str)
+    for _, value in ipairs(tbl) do
+        if value == str then
+            return true
+        end
+    end
+    return false
+end
+
 function M.lsp_icons()
     local signs = { Error = " ", Warn = "", Hint = "", Info = "" }
     for type, icon in pairs(signs) do
