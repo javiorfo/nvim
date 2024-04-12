da1d95e4cf1425aea44d6d81f34474630c740627
Javier Orfo <124217829+javiorfo@users.noreply.github.com>
2024-04-12 20:09:22 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,13 +4,11 @@ vim.opt.number = true
 vim.opt.autoindent = true
 vim.opt.ignorecase = true
 vim.opt.termguicolors = true
-
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smarttab = true
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
-
 vim.opt.mouse = "a"
 vim.opt.hidden = true
 vim.opt.cursorline = true
