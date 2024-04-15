67ab8b70ccfa9ee9ec65987211bf289b7ce49cdd
Javier Orfo <124217829+javiorfo@users.noreply.github.com>
2024-04-15 17:00:56 -0300

    Update ship.lua

--- lua/plugins/ship.lua
+++ lua/plugins/ship.lua
@@ -3,20 +3,18 @@ return {
     lazy = true,
     ft = "ship",
     cmd = { "ShipCreate", "ShipCreateEnv" },
-    dependencies = { 
+    dependencies = {
         "javiorfo/nvim-popcorn",
         "javiorfo/nvim-spinetta",
         "hrsh7th/nvim-cmp"
     },
-    config = function()
-        require 'ship'.setup {
-            response = {
-                show_headers = 'res',
-                window_type = 'p',
-                size = 30
-            },
+    opts = {
+        response = {
+            show_headers = 'res',
+            window_type = 'p',
+            size = 30
         }
-    end,
+    },
     keys = {
         { "<leader>sh", "<cmd>Ship<cr>" },
         { "<leader>sc", "<cmd>ShipCloseResponse<cr>" }
