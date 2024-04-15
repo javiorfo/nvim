02b243d07b7946af6ddeeb3444bf17303389c461
Javier Orfo <124217829+javiorfo@users.noreply.github.com>
2024-04-15 16:59:01 -0300

    Update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,9 @@
 return {
     "nvim-lualine/lualine.nvim",
     lazy = false,
-    config = function()
-        require 'lualine'.setup {
-            options = {
-                disabled_filetypes = { 'NvimTree' }
-            }
+    opts = {
+        options = {
+            disabled_filetypes = { 'NvimTree' }
         }
-    end
+    }
 }
