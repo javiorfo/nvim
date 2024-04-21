82ded526874ed72260f9d9217ef8623b617280e2
Javier Orfo <javierorfo@prontonmail.com>
2024-04-21 13:29:41 -0300

    springtime update

--- lua/plugins/springtime.lua
+++ lua/plugins/springtime.lua
@@ -1,11 +1,18 @@
 return {
     "javiorfo/nvim-springtime",
     lazy = true,
-    cmd = { "Springtime", "SpringtimeUpdate" },
---     build = ":SpringtimeBuild",
+    cmd = { "Springtime", "SpringtimeUpdate", "SpringtimeBuild" },
+    build = ":SpringtimeBuild",
     dependencies = {
         "javiorfo/nvim-popcorn",
         "javiorfo/nvim-spinetta",
         "hrsh7th/nvim-cmp"
     },
+    opts = {
+        spring = {
+            java_version = {
+                selected = 2
+            }
+        }
+    }
 }
