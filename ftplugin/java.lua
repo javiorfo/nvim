0a606e7e131346cf0d9b0534298fe254d752754a
Javier Orfo <javierorfo@prontonmail.com>
2024-05-02 09:57:46 -0300

    java runtimes

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,8 +1,8 @@
 local home = os.getenv("HOME")
-local jdtls = require'jdtls'
-local dap = require'dap'
+local jdtls = require 'jdtls'
+local dap = require 'dap'
 
-require'settings.util'.lsp_icons()
+require 'settings.util'.lsp_icons()
 
 local config = {
     ---@diagnostic disable-next-line: undefined-global
@@ -14,12 +14,24 @@ local config = {
     },
     root_dir = jdtls.setup.find_root({ '.git', 'mvnw', 'gradlew' }),
     settings = {
-        java = {}
+        java = {
+            codeGeneration = {
+                toString = {
+                    template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+                }
+            },
+            configuration = {
+                runtimes = {
+                    --                     { name = "JavaSE-1.8", path = "/usr/lib/jvm/java-8-openjdk" },
+                    { name = "JavaSE-21", path = "/usr/lib/jvm/java-21-openjdk" },
+                }
+            }
+        }
     },
     init_options = {
         bundles = {
             vim.fn.glob(
-            "~/.config/nvim/servers/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.50.0.jar")
+                "~/.config/nvim/servers/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.50.0.jar")
         }
     },
     on_attach = function(client, _)
