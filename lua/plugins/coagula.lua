16952a5be038981b668b5cba7afee3f63490c3d8
Javier Orfo <javierorfo@prontonmail.com>
2024-05-11 10:36:57 -0300

    coagula added

---
+++ lua/plugins/coagula.lua
@@ -0,0 +1,11 @@
+return {
+    "javiorfo/nvim-coagula",
+    lazy = true,
+    enabled = true,
+    cmd = { "Coagula" },
+    dependencies = {
+        "javiorfo/nvim-popcorn",
+        "javiorfo/nvim-spinetta",
+        "hrsh7th/nvim-cmp"
+    }
+}
